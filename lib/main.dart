3923333a6f1448e31f426a1b66028b32e3b1a055
feoolga <kirianova_ov@mail.ru>
2026-09-24 18:41:18 +0300

    chore(logger): удалены пустые заглушки errorsOnly и all и явно указана NDK 27 для совместимости с file_picker

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/splash_screen.dart';
 import 'theme/app_theme.dart';
-import 'package:flutter/foundation.dart' show kReleaseMode;
-import 'core/logger/app_logger.dart';
 
 void main() async {
   // Загружаем .env файл перед запуском приложения
   await dotenv.load(fileName: ".env");
-
-  // Настройка логирования
-  if (kReleaseMode) {
-    AppLogger.errorsOnly(); // В production — только ошибки
-  } else {
-    AppLogger.all(); // В разработке — всё
-  }
 
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -28,10 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'AI Assistant',
       theme: AppTheme.lightTheme,
-      // theme: ThemeData.dark(),
-      // home: const TestScreen(),
       home: const SplashScreen(),
-      // home: const ChatScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
